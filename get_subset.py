fd7b55b917658d47d074e6756f8dc24dd164d825
Dr-Vphuc <ngphuc689@gmail.com>
2026-05-02 09:51:26 +0700

    chore: update subset dataa

--- get_subset.py
+++ get_subset.py
@@ -7,6 +7,10 @@ df = pd.read_csv('processed_data/encoded_ai_company_adoption.csv')
 subset_10k = df.sample(n=10000, random_state=42)
 subset_10k.to_csv('./processed_data/subset_10k.csv', index=False)
 
+# Sample 20,000 random records
+subset_20k = df.sample(n=20000, random_state=43)
+subset_20k.to_csv('./processed_data/subset_20k.csv', index=False)
+
 # Sample 50,000 random records
-subset_50k = df.sample(n=50000, random_state=43)
+subset_50k = df.sample(n=50000, random_state=44)
 subset_50k.to_csv('./processed_data/subset_50k.csv', index=False)
